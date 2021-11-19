385f073344c0df81c5db39d5ec57b1ee1876f105
Igor Polousov <igorpolousov@gmail.com>
2021-11-19 15:07:32 +0300

    + changes to LoadViewController

--- Extension/LoadViewController.swift
+++ Extension/LoadViewController.swift
@@ -9,23 +9,25 @@ import UIKit
 
 class LoadViewController: UITableViewController {
     
+    var savedScripts = [UserScript]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    title = "Saved Scripts"
         
         
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return savedScripts.count
     }
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let script = savedScripts[indexPath.row]
         
-        // Configure the cell...
-        
+        cell.textLabel?.text = script.title
         return cell
     }
     
@@ -34,10 +36,12 @@ class LoadViewController: UITableViewController {
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            if savedScripts.isEmpty {
+                if let vc = storyboard?.instantiateViewController(withIdentifier: "LoadScript") as? ActionViewController {
+                    navigationController?.pushViewController(vc, animated: true)
+                }
+            }
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }
     }
     
