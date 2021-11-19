aa0a9a374f8fadea5e0f25b80afd0955fa0b4029
Igor Polousov <igorpolousov@gmail.com>
2021-11-19 13:53:25 +0300

    + scriptExamples

---
+++ Extension/ExampleScripts.swift
@@ -0,0 +1,18 @@
+//
+//  ExampleScripts.swift
+//  Extension
+//
+//  Created by Igor Polousov on 19.11.2021.
+//
+
+import Foundation
+
+let exampleScripts = [
+    (title: "Alert",
+     exampleScript: "alert(document.title);"),
+    
+    (title: "ExtendedAlert",
+     exampleScript: """
+            alert("Page title: " + document.title + "\\nPage url: " + document.URL);")
+            """)
+]
